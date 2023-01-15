d5c4f46b00fc2bf826e7337d6f5b9d1eabc560ed
ELBASTIDAAS <miguel.bastidas18@tectijuana.edu.mx>
2023-01-15 12:03:09 -0800

    class-1

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+# LOGOUT_REDIRECT_URL = 'home'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", include("pages.urls")),
     path("posts/", include("posts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
